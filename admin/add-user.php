cafc935fa31ef23ab9b1c33a6a39ea63a0f0cc06
ttanimarefin <ttanimarefin@gmail.com>
2021-06-08 01:34:43 +0600

    dynamic add user

--- admin/add-user.php
+++ admin/add-user.php
@@ -6,8 +6,42 @@
                   <h1 class="admin-heading">Add User</h1>
               </div>
               <div class="col-md-offset-3 col-md-6">
+                  
+                  <?php
+                    if(isset($_POST['submit'])){
+                        include 'config.php';
+                        $fname=mysqli_real_escape_string($connection,$_POST['fname']);
+                        $lname=mysqli_real_escape_string($connection,$_POST['lname']);
+                        $user=mysqli_real_escape_string($connection,$_POST['user']);
+                        $password=mysqli_real_escape_string($connection,md5($_POST['password']));
+                        $role=mysqli_real_escape_string($connection,$_POST['role']);
+                        
+                        $query="SELECT username FROM user WHERE username='$user' ";
+                        $result=mysqli_query($connection,$query) or die("Query Faild.");
+                        
+                        $count=mysqli_num_rows($result);
+                        
+                        if($count>0){
+                            echo "Username Already Exists";
+                        }{
+                            $query1="INSERT INTO user(first_name,last_name,username,password,role) VALUE('$fname','$lname','$user','$password','$role')";
+                            $result=mysqli_query($connection,$query1) or die("Query Faild.");
+                            
+
+                            if($result){
+                                header("location: users.php");
+                            }
+                        }
+                    }
+                  
+                  
+                  
+                  
+                  
+                  ?>
+                  
                   <!-- Form Start -->
-                  <form  action="" method ="POST" autocomplete="off">
+                  <form  action="<?php $_SERVER['PHP_SELF']?>" method ="POST" autocomplete="off">
                       <div class="form-group">
                           <label>First Name</label>
                           <input type="text" name="fname" class="form-control" placeholder="First Name" required>
---
+++ admin/config.php
@@ -0,0 +1,4 @@
+<?php
+$connection=mysqli_connect('localhost','root','','news_project') or die("Not Connected".mysqli_error());
+
+?>
