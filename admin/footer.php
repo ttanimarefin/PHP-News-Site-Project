4caaa686587fef799fa5b2f242c9454d4406f216
ttanimarefin <ttanimarefin@gmail.com>
2021-06-07 20:38:34 +0600

    template

---
+++ admin/footer.php
@@ -0,0 +1,13 @@
+<!-- Footer -->
+<div id ="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <span>© Copyright 2021 News | Powered by <a href="">Tanim Arefin</a></span>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /Footer -->
+</body>
+</html>
