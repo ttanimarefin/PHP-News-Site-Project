b465b97f0125f8f9ec27fcd9398e2284fcd7b3ba
ttanimarefin <ttanimarefin@gmail.com>
2021-06-27 00:13:51 +0600

    change FILES img>image name

--- admin/save-update-post.php
+++ admin/save-update-post.php
@@ -1,8 +1,9 @@
 <?php
 include "config.php";
+print_r($_POST['old-image']);
 
-if(empty($_FILES['new-img']['name'])){
-    $new_name = $_POST['old_image'];
+if(empty($_FILES['new-image']['name'])){
+    $new_name = $_POST['old-image'];
 }else{
     $errors=array();
 
@@ -26,6 +27,7 @@ if($file_size > 2097152){
 }
 $new_name= time(). "-".basename($file_name);
 $target= "upload/".$new_name;
+$image_name=$new_name;
 
 
 if(empty($errors)==true){
@@ -36,9 +38,19 @@ if(empty($errors)==true){
 }
 }
 
-$query= "UPDATE post SET title= '{["post_title"]}';
+$query= "UPDATE post SET 
+       title= '{$_POST["post_title"]}',description='{$_POST["postdesc"]}', category={$_POST["category"]},post_img='{$image_name}' WHERE post_id={$_POST["post_id"]};";
 
-
+if($_POST['old_category']!=$_POST["category"]){
+    $query.="UPDATE category SET post=post-1 WHERE category_id={$_POST['old_category']};";
+    $query.="UPDATE category SET post=post+1 WHERE category_id={$_POST['old_category']};";
+}
+$result=mysqli_multi_query($connection,$query);
+if($result){
+    header("location:../admin/post.php");
+}else{
+    echo "Query Failed";
+}
 
 
 
